56ebde023448585b81b5d678b1de9230763aca81
KangHoJ <rkdghwjd1999@gmail.com>
2024-03-12 15:24:33 +0900

    key관리 반영

--- dataload.py
+++ dataload.py
@@ -1,11 +1,15 @@
 import requests
 import pandas as pd
 from preporcessing import category
+from dotenv import load_dotenv
+import os
+load_dotenv('env/key.env')
+key = os.getenv('API_KEY')
 
 
-# API 요청을 자동화하여 데이터 가져오기
+#API 요청을 자동화하여 데이터 가져오기
 def get_data(start, end):
-    url = f'http://openapi.seoul.go.kr:8088/키/json/GarakGradePrice/{start}/{end}/'
+    url = f'http://openapi.seoul.go.kr:8088/{key}/json/GarakGradePrice/{start}/{end}/'
     response = requests.get(url)
     data = response.json()
     return pd.DataFrame(data['GarakGradePrice']['row'])
--- sql.py
+++ sql.py
@@ -1,22 +1,23 @@
 import pymysql
 from sqlalchemy import create_engine
 from dataload import main_load
+from env import settings
 
 df = main_load()
 
 def db_import():
-    host = 'localhost'
-    port = 3306
-    user = ''
-    password = ''
-    database = ''
+    host = settings.DATABASE_CONFIG['host']
+    port = settings.DATABASE_CONFIG['port']
+    user = settings.DATABASE_CONFIG['user']
+    password = settings.DATABASE_CONFIG['password']
+    database = settings.DATABASE_CONFIG['database']
 
     engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}')
     conn = engine.connect()
     print('연결 성공')
     df.to_sql(name='sijang3', con=conn, if_exists='replace', index=False) # 새로운 db 데이터 
     print("새로운 db 데이터 넣기 성공.")
-    df.to_sql(name='sijang4', con=conn, if_exists='replace', index=False) # Append new data
+    df.to_sql(name='sijang4', con=conn, if_exists='append', index=False) # Append new data
     print("새로운 데이터 넣기 성공.")
     
 
